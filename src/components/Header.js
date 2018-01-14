362986d4f817c7a771f7bd2d5698788aec155cd4
Jatin Kumar <jatn15011999@gmail.com>
2018-01-14 19:55:54 +0530

    Add private routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import {NavLink} from 'react-router-dom';
 import React from 'react';
+import {connect} from 'react-redux';
+import {startLogout} from '../actions/auth';
 
-const Header = ()=>(
-    <div>
+export const Header = ({startLogout})=>(
+    <header>
         <h1>Expensify</h1>
-        <p>
-            <NavLink activeClassName='is-active' to="/" exact={true}>Home</NavLink>
-            <NavLink activeClassName='is-active' to="/create">Create</NavLink>
-            {/*<NavLink activeClassName='is-active' to="/edit">Edit</NavLink>*/}
-            <NavLink activeClassName='is-active' to="/help">Help</NavLink>
-        </p>
-    </div>
+        <NavLink activeClassName='is-active' to="/" exact={true}>Home</NavLink>
+        <NavLink activeClassName='is-active' to="/create">Create</NavLink>
+        {/*<NavLink activeClassName='is-active' to="/edit">Edit</NavLink>*/}
+        {/*<NavLink activeClassName='is-active' to="/help">Help</NavLink>*/}
+        <button onClick={startLogout}>Logout</button>
+    </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch)=>({
+    startLogout : ()=>dispatch(startLogout())
+});
+
+export default connect(undefined,mapDispatchToProps)(Header);
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,29 @@
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import {Router, Route, Switch, Link, NavLink} from 'react-router-dom';
 import React from 'react';
+import createHistory from 'history/createBrowserHistory';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import Header from '../components/Header';
 import HelpExpensePage from '../components/HelpExpensePage';
 import NotFoundPage from '../components/NotFoundPage';
+import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
+
+export const history = createHistory();
 
 const AppRouter = ()=>(
-    <BrowserRouter>
+    <Router history={history}>
         <div>
-            <Header/>
             <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit/:id" component={EditExpensePage} />
+                <Route path="/" component={LoginPage} exact={true}/>
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpExpensePage} />
                 <Route component={NotFoundPage}/>
             </Switch>
         </div>
-    </BrowserRouter>
+    </Router>
 );
 
 export default AppRouter ;
